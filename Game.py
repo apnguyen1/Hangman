11d43e8a0615769e4869e5c7ad035a9642c2b1f0
Andrew Nguyen <andrewnguyen17678@gmail.com>
2022-07-14 22:35:09 -0700

    testing first Flask route

--- Game.py
+++ Game.py
@@ -1,33 +1,11 @@
-# import Hangman Game Client
-from gamecli import *
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def test():
+    return "<p>Hello World<p>"
+
 
 if __name__ == "__main__":
-    file = "words.txt"
-    
-    def dictProc(file):
-        dict = list()
-        
-        with open(file, "r") as file:
-            words = file.readlines()
-            
-            for word in words:
-                dict.append(word.strip())
-
-        return dict
-    
-    lettersLen = int(input("Hello, Welcome to a game of Hangman. How many letters do you want to guess in this's games word?: "))
-    
-    
-    dict = dictProc(file)
-    
-    # game = Manager.Manager(dict, lettersLen)
-    game = Manager(dict,lettersLen)    
-    # game.randomize()
-    
-    word = game.randomize()
-    
-    print(word)
-    
-    game.guess("E", word)
-    
-    print()
+    app.run(debug=True)
